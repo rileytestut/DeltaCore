dd25a032221267b3283d1a8b23ee57c430492367
Riley Testut <Riley@Rileys-MacBook-Pro.local>
2015-03-08 23:07:37 -0700

    Initial Commit

---
+++ DeltaCore/DeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  DeltaCore.h
+//  DeltaCore
+//
+//  Created by Riley Testut on 3/8/15.
+//  Copyright (c) 2015 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeltaCore.
+FOUNDATION_EXPORT double DeltaCoreVersionNumber;
+
+//! Project version string for DeltaCore.
+FOUNDATION_EXPORT const unsigned char DeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeltaCore/PublicHeader.h>
+
+

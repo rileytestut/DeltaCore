cc8a9bff8440463ad5cc0e919caf047e4dc1463c
Riley Testut <riley@rileytestut.com>
2016-06-05 03:01:00 -0500

    Adds support for rendering BGRA8 pixel formats

--- DeltaCore/VideoManager.swift
+++ DeltaCore/VideoManager.swift
@@ -14,6 +14,7 @@ public extension VideoManager
     public struct BufferInfo
     {
         public let inputFormat: Format
+        public let outputFormat: Format
         
         public let inputDimensions: CGSize
         public let outputDimensions: CGSize
@@ -22,13 +23,15 @@ public extension VideoManager
             return Int(self.inputDimensions.width * self.inputDimensions.height) * self.inputFormat.bytesPerPixel
         }
         public var outputBufferSize: Int {
-            return Int(self.inputDimensions.width * self.inputDimensions.height) * Format.RGBA8.bytesPerPixel
+            return Int(self.inputDimensions.width * self.inputDimensions.height) * self.outputFormat.bytesPerPixel
         }
         
         public init(inputFormat: Format, inputDimensions: CGSize, outputDimensions: CGSize)
         {
             self.inputFormat = inputFormat
             self.inputDimensions = inputDimensions
+            
+            self.outputFormat = .BGRA8
             self.outputDimensions = outputDimensions
         }
     }
@@ -39,13 +42,13 @@ public extension VideoManager.BufferInfo
     public enum Format
     {
         case RGB565
-        case RGBA8
-        
+        case BGRA8
+
         public var bytesPerPixel: Int {
             switch self
             {
             case .RGB565: return 2
-            case .RGBA8: return 4
+            case .BGRA8: return 4
             }
         }
     }
@@ -61,8 +64,6 @@ public class VideoManager: NSObject, DLTAVideoRendering
     
     public let videoBuffer: UnsafeMutablePointer<UInt8>
     private let convertedVideoBuffer: UnsafeMutablePointer<UInt8>
-    
-    private let outputFormat = BufferInfo.Format.RGBA8
     
     public init(bufferInfo: BufferInfo)
     {
@@ -107,24 +108,28 @@ internal extension VideoManager
             
             var inputVImageBuffer = vImage_Buffer(data: self.videoBuffer, height: vImagePixelCount(self.bufferInfo.inputDimensions.height), width: vImagePixelCount(self.bufferInfo.inputDimensions.width), rowBytes: self.bufferInfo.inputFormat.bytesPerPixel * Int(self.bufferInfo.inputDimensions.width))
             
+            let bitmapBuffer: UnsafeMutablePointer<UInt8>
             var convertedVImageBuffer: vImage_Buffer
-            if self.bufferInfo.inputFormat == .RGBA8
+            
+            if self.bufferInfo.inputFormat == .BGRA8
             {
+                bitmapBuffer = self.videoBuffer
                 convertedVImageBuffer = inputVImageBuffer
             }
             else
             {
-                convertedVImageBuffer = vImage_Buffer(data: self.convertedVideoBuffer, height: vImagePixelCount(self.bufferInfo.inputDimensions.height), width: vImagePixelCount(self.bufferInfo.inputDimensions.width), rowBytes: self.outputFormat.bytesPerPixel * Int(self.bufferInfo.inputDimensions.width))
+                bitmapBuffer = self.convertedVideoBuffer
+                convertedVImageBuffer = vImage_Buffer(data: self.convertedVideoBuffer, height: vImagePixelCount(self.bufferInfo.inputDimensions.height), width: vImagePixelCount(self.bufferInfo.inputDimensions.width), rowBytes: self.bufferInfo.outputFormat.bytesPerPixel * Int(self.bufferInfo.inputDimensions.width))
             }
                         
             switch self.bufferInfo.inputFormat
             {
-            case .RGB565: vImageConvert_RGB565toRGBA8888(255, &inputVImageBuffer, &convertedVImageBuffer, 0)
-            case .RGBA8: break
+            case .RGB565: vImageConvert_RGB565toBGRA8888(255, &inputVImageBuffer, &convertedVImageBuffer, 0)
+            case .BGRA8: break
             }
             
-            let bitmapData = NSData(bytes: self.convertedVideoBuffer, length: self.bufferInfo.outputBufferSize)
-            let image = CIImage(bitmapData: bitmapData, bytesPerRow: self.outputFormat.bytesPerPixel * Int(self.bufferInfo.inputDimensions.width), size: self.bufferInfo.outputDimensions, format: kCIFormatRGBA8, colorSpace: nil)
+            let bitmapData = NSData(bytes: bitmapBuffer, length: self.bufferInfo.outputBufferSize)
+            let image = CIImage(bitmapData: bitmapData, bytesPerRow: self.bufferInfo.outputFormat.bytesPerPixel * Int(self.bufferInfo.inputDimensions.width), size: self.bufferInfo.outputDimensions, format: kCIFormatBGRA8, colorSpace: nil)
             
             for gameView in self.gameViews
             {

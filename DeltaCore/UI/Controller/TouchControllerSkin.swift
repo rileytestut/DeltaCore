041293d2fdd9d607ec7c572ca2993e1dafe69d1b
Riley Testut <riley@rileytestut.com>
2022-08-09 15:36:39 -0500

    Fixes TouchControllerSkin when in Split View
    We now use `app` placement to dramatically simplify the calculations necessary to position screens (and inputs).

--- DeltaCore/UI/Controller/TouchControllerSkin.swift
+++ DeltaCore/UI/Controller/TouchControllerSkin.swift
@@ -26,7 +26,6 @@ public struct TouchControllerSkin
     public var isDebugModeEnabled: Bool { false }
     
     public var screenLayoutAxis: LayoutAxis = .vertical
-    public var layoutGuide: UILayoutGuide?
     
     private let controllerSkin: ControllerSkin
     
@@ -56,52 +55,17 @@ extension TouchControllerSkin: ControllerSkinProtocol
     public func items(for traits: ControllerSkin.Traits) -> [ControllerSkin.Item]?
     {
         guard
-            let fullScreenFrame = self.fullScreenFrame(for: traits),
-            var touchScreenItem = self.controllerSkin.items(for: traits)?.first(where: { $0.kind == .touchScreen })
+            var touchScreenItem = self.controllerSkin.items(for: traits)?.first(where: { $0.kind == .touchScreen }),
+            let screens = self.screens(for: traits), screens.count > 1
         else { return nil }
         
-        let touchScreenFrame: CGRect
+        let screen = screens[1]
         
-        if let touchScreenIndex = self.controllerSkin.screens(for: traits)?.firstIndex(where: { touchScreenItem.frame.contains($0.outputFrame) }),
-           let updatedScreen = self.screens(for: traits)?[touchScreenIndex]
-        {
-            // This (original) screen is completely covered by a touch screen input,
-            // therefore we assume it's a touch screen and use its (updated) frame.
-            touchScreenFrame = updatedScreen.outputFrame
-        }
-        else
-        {
-            // The touch screen (if one exists) is partially covering the main screen,
-            // so calculate the intersection to determine where to place touch screen.
-            
-            var screenFrame = self.controllerSkin.gameScreenFrame(for: traits) ?? fullScreenFrame
-            var itemFrame = touchScreenItem.frame
-            
-            // Align itemFrame relative to screenFrame's origin.
-            itemFrame.origin.x -= screenFrame.minX
-            itemFrame.origin.y -= screenFrame.minY
-            screenFrame.origin = .zero
-            
-            var intersection = screenFrame.intersection(itemFrame)
-            intersection.origin.x /= screenFrame.width
-            intersection.origin.y /= screenFrame.height
-            intersection.size.width /= screenFrame.width
-            intersection.size.height /= screenFrame.height
-            
-            intersection.origin.x *= fullScreenFrame.width
-            intersection.origin.y *= fullScreenFrame.height
-            intersection.size.width *= fullScreenFrame.width
-            intersection.size.height *= fullScreenFrame.height
-            
-            // Translate intersection back to correct location.
-            intersection.origin.x += fullScreenFrame.minX
-            intersection.origin.y += fullScreenFrame.minY
-            
-            touchScreenFrame = intersection
-        }
+        // For now, we assume the touch screen is always the second screen, and that touchScreenItem should completely cover it.
         
-        touchScreenItem.frame = touchScreenFrame
-        touchScreenItem.extendedFrame = touchScreenFrame
+        touchScreenItem.placement = .app
+        touchScreenItem.frame = screen.outputFrame
+        touchScreenItem.extendedFrame = screen.outputFrame
         return [touchScreenItem]
     }
     
@@ -112,22 +76,18 @@ extension TouchControllerSkin: ControllerSkinProtocol
 
     public func screens(for traits: ControllerSkin.Traits) -> [ControllerSkin.Screen]?
     {
-        guard let fullScreenFrame = self.fullScreenFrame(for: traits) else { return nil }
-
         let screensCount = CGFloat(self.controllerSkin.screens(for: traits)?.count ?? 0)
         
         let screens = self.controllerSkin.screens(for: traits)?.enumerated().map { (index, screen) -> ControllerSkin.Screen in
+            let length = 1.0 / screensCount
+            
             var screen = screen
+            screen.placement = .app
             
             switch self.screenLayoutAxis
             {
-            case .horizontal:
-                let length = fullScreenFrame.width / screensCount
-                screen.outputFrame = CGRect(x: fullScreenFrame.minX + length * CGFloat(index), y: fullScreenFrame.minY, width: length, height: fullScreenFrame.height)
-                
-            case .vertical:
-                let length = fullScreenFrame.height / screensCount
-                screen.outputFrame = CGRect(x: fullScreenFrame.minX, y: fullScreenFrame.minY + length * CGFloat(index), width: fullScreenFrame.width, height: length)
+            case .horizontal: screen.outputFrame = CGRect(x: length * CGFloat(index), y: 0, width: 0.5, height: 1.0)
+            case .vertical: screen.outputFrame = CGRect(x: 0, y: length * CGFloat(index), width: 1.0, height: 0.5)
             }
             
             return screen
@@ -139,60 +99,5 @@ extension TouchControllerSkin: ControllerSkinProtocol
     public func aspectRatio(for traits: ControllerSkin.Traits) -> CGSize?
     {
         return self.controllerSkin.aspectRatio(for: traits)
-    }
-}
-
-private extension TouchControllerSkin
-{
-    func fullScreenFrame(for traits: ControllerSkin.Traits) -> CGRect?
-    {
-        // Assumes skin is a full screen skin.
-        
-        guard let mappingSize = self.aspectRatio(for: traits), let core = Delta.core(for: self.gameType) else { return nil }
-        
-        let fullScreenSize: CGSize
-        
-        if let screens = self.controllerSkin.screens(for: traits), let outputFrame = screens.first?.outputFrame
-        {
-            var size = CGSize(width: outputFrame.width * mappingSize.width, height: outputFrame.height * mappingSize.height)
-            
-            // Assume screens are the same size.
-            switch self.screenLayoutAxis
-            {
-            case .horizontal: size.width *= CGFloat(screens.count)
-            case .vertical: size.height *= CGFloat(screens.count)
-            }
-            
-            fullScreenSize = size
-        }
-        else
-        {
-            fullScreenSize = core.videoFormat.dimensions
-        }
-        
-        let layoutFrame: CGRect
-        let containingSize: CGSize
-        
-        if let layoutGuide = self.layoutGuide, let bounds = layoutGuide.owningView?.bounds
-        {
-            var frame = layoutGuide.layoutFrame
-            frame.size.height = bounds.height - frame.minY // HACK: Ignore bottom insets to prevent home indicator messing up calculations.
-            
-            layoutFrame = frame
-            containingSize = bounds.size
-        }
-        else
-        {
-            layoutFrame = CGRect(origin: .zero, size: mappingSize)
-            containingSize = layoutFrame.size
-        }
-        
-        var screenFrame = AVMakeRect(aspectRatio: fullScreenSize, insideRect: layoutFrame)
-        screenFrame.origin.x /= containingSize.width
-        screenFrame.origin.y /= containingSize.height
-        screenFrame.size.width /= containingSize.width
-        screenFrame.size.height /= containingSize.height
-
-        return screenFrame
     }
 }
